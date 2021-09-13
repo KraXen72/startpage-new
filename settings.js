8f30bbc77942ed1e8b544f5dc371abda18942184
KraXen72 <dpelech1@gmail.com>
2021-09-13 10:37:29 +0200

    refactor initialization of setting object

--- settings.js
+++ settings.js
@@ -1,21 +1,135 @@
 //TODO update these settings' values when i load the json from localstorage (before generating load from localstorage)
 //update from localstorage here
 
+const settings = {
+    s: {
+        TXT_TITLE: {title: "Settings", type: 'heading'},
+        connect: {
+            title: `Connect columns`,
+            desc: `connects link columns together`,
+            type: 'bool',
+            key: 'connect',
+            classes: ['connect', '']
+        },
+        compact: {
+            title: `Compact links`,
+            desc: `make links not take up as much space`,
+            type: 'bool',
+            key: 'compact',
+            classes: ['compact', '']
+        },
+        leftpic: {
+            title: `Move Image to left`,
+            desc: `Move image to left instead of top. will have no effect if image is hidden.`,
+            type: 'bool',
+            key: 'leftpic',
+            classes: ['leftpic', '']
+        },
+        tallpic: {
+            title: `Tall left image`,
+            desc: `(if Image is on the left) make the image tall (for portrait images)`,
+            type: 'bool',
+            key: 'tallpic',
+            classes: ['tallpic', '']
+        },
+        slim: {
+            title: `Slim container`,
+            desc: `max width of container is now 32rem instaed of 40rem`,
+            type: 'bool',
+            key: 'slim',
+            classes: ['slim', '']
+        },
+        cols: {
+            title: `Number of columns`,
+            desc: `How many columns to show: either 2 or 3`,
+            type: 'sel',
+            opts: [3,2],
+            key: 'cols',
+            classes: ['cols-3', 'cols-2']
+        },
+        verdana: {
+            title: `Use Verdana font`,
+            desc: `Use Verdana font instead of Roboto`,
+            type: 'bool',
+            key: 'verdana',
+            classes: ['verdana', '']
+        },
+        TXT_HIDING: {title: "Hiding elements", type: 'heading'},
+        nosearch: {
+            title: `Hide Search`,
+            desc: '',
+            type: 'bool',
+            key: 'nosearch',
+            classes: ['nosearch', '']
+        },
+        nopic: {
+            title: `Hide Image`,
+            desc: '',
+            type: 'bool',
+            key: 'nopic',
+            classes: ['nopic', '']
+        },
+        notitle: {
+            title: `Hide Title`,
+            desc: '',
+            key: 'notitle',
+            type: 'bool',
+            classes: ['notitle', '']
+        },
+        nogreeting: {
+            title: `Hide Greeting`,
+            desc: '',
+            key: 'nogreeting',
+            type: 'bool',
+            classes: ['nogreeting', '']
+        }
+    }
+}
+
 /**
  * creates a new Setting element
  * @classdesc DOM element for a certain setting element
  */
 class SettingElem {
     //s-update is the class for element to watch
-    //updateKey is the key to get checked when writing an update, for checkboxes it's checked, for selects its value etc.
-    //updateMethod is the eventlistener to use. for checkbox its be onclick, for select its be onchange etc.
-    //mutable: if this setting actually changes something and can be updated. titles are immutable
-    //props: save the props from constructor to this class
-    //type: bool, sel, heading etc
     constructor (props) {
+        console.log(props)
+        /**
+         * is the key to get checked when writing an update, for checkboxes it's checked, for selects its value etc. 
+         * @type {String} 
+         */
+        this.updateKey = ''
+
+        /**
+         * is the eventlistener to use. for checkbox its be onclick, for select its be onchange etc.
+         * @type {String}
+         */
+        this.updateMethod = ''
+
+        /**
+         * save the props from constructor to this class (instance)
+         * @type {Object}
+         */
         this.props = props
+
+        /**
+         * type of this settingElem, can be {'bool' | 'sel' | 'heading'}
+         * @type {String} 
+         */
         this.type = props.type
-        
+
+        /**
+         * if this setting actually changes something and can be updated. titles are immutable
+         * @type {Boolean}
+         */
+        this.mutable = false
+
+        /**
+         * innerHTML for settingElement
+         * @type {String}
+         */
+        this.HTML = ''
+
         switch (props.type) {
             case 'bool':
                 this.HTML = `<span class="setting-title" title="${props.desc}">${props.title}</span> 
@@ -54,7 +168,7 @@ class SettingElem {
     //this reflects the changes on the actual layout
     /**
      * update the layout through updating global container object
-     * @param {Object} elemAndCb {elem: element to get setting from, callback: callback or 'normal' for normal callback}
+     * @param {{elem: Element, callback: 'normal'|Function}} elemAndCb
      */
     set update({elem, callback}) {
         let target = elem.getElementsByClassName('s-update')[0]
@@ -69,7 +183,10 @@ class SettingElem {
         
     }
 
-    //this initializes the element and its eventlisteners
+    /**
+     * this initializes the element and its eventlisteners
+     * @returns {Element}
+    */
     get elem() {
         let w = document.createElement('div')
         w.classList.add("setting")
@@ -79,7 +196,6 @@ class SettingElem {
 
         //add an eventlistener if the setting is mutable
         if (this.mutable) {
-            console.log(this.updateMethod)
             w[this.updateMethod] = () => {
                 this.update = {elem: w, callback: 'normal'}
             }
@@ -93,106 +209,16 @@ class SettingElem {
  * initialize settings elements. only call after DOM init. only call once.
  */
 function initsettings() {
-    const settings = {
-        leftpic: false, //.leftpic: put the pic to the left instead of top
-        tallpic: false, //.tallpic: pic will be yahallo (358*279.72px) tall and wide (only works with leftpic)
-    
-        s: {
-            TXT_TITLE: {title: "Settings", type: 'heading'},
-            connect: {
-                value: Container.p["connect"],
-                title: `Connect columns`,
-                desc: `connects link columns together`,
-                type: 'bool',
-                key: 'connect',
-                classes: ['connect', '']
-            },
-            compact: {
-                value: Container.p["compact"],
-                title: `Compact links`,
-                desc: `make links not take up as much space`,
-                type: 'bool',
-                key: 'compact',
-                classes: ['compact', '']
-            },
-            leftpic: {
-                value: Container.p["leftpic"],
-                title: `Move Image to left`,
-                desc: `Move image to left instead of top. will have no effect if image is hidden.`,
-                type: 'bool',
-                key: 'leftpic',
-                classes: ['leftpic', '']
-            },
-            tallpic: {
-                value: Container.p["tallpic"],
-                title: `Tall left image`,
-                desc: `(if Image is on the left) make the image tall (for portrait images)`,
-                type: 'bool',
-                key: 'tallpic',
-                classes: ['tallpic', '']
-            },
-            slim: {
-                value: Container.p["slim"],
-                title: `Slim container`,
-                desc: `max width of container is now 32rem instaed of 40rem`,
-                type: 'bool',
-                key: 'slim',
-                classes: ['slim', '']
-            },
-            cols: {
-                value: Container.p["cols"],
-                title: `Number of columns`,
-                desc: `How many columns to show: either 2 or 3`,
-                type: 'sel',
-                opts: [3,2],
-                key: 'cols',
-                classes: ['cols-3', 'cols-2']
-            },
-            verdana: {
-                value: Container.p["verdana"],
-                title: `Use Verdana font`,
-                desc: `Use Verdana font instead of Roboto`,
-                type: 'bool',
-                key: 'verdana',
-                classes: ['verdana', '']
-            },
-            TXT_HIDING: {title: "Hiding elements", type: 'heading'},
-            nosearch: {
-                value: Container.p["nosearch"],
-                title: `Hide Search`,
-                desc: '',
-                type: 'bool',
-                key: 'nosearch',
-                classes: ['nosearch', '']
-            },
-            nopic: {
-                value: Container.p["nopic"],
-                title: `Hide Image`,
-                desc: '',
-                type: 'bool',
-                key: 'nopic',
-                classes: ['nopic', '']
-            },
-            notitle: {
-                value: Container.p["notitle"],
-                title: `Hide Title`,
-                desc: '',
-                key: 'notitle',
-                type: 'bool',
-                classes: ['notitle', '']
-            },
-            nogreeting: {
-                value: Container.p["nogreeting"],
-                title: `Hide Greeting`,
-                desc: '',
-                key: 'nogreeting',
-                type: 'bool',
-                classes: ['nogreeting', '']
-            }
-        },
-        //add option to generate settings elements here by type + add some headings
+    //add value key to all settings from the container (after container is initialized)
+    for (let i = 0; i < Object.keys(settings.s).length; i++) {
+        const setting = settings.s[Object.keys(settings.s)[i]];
+
+        if (setting.type === 'bool' || setting.type === 'sel') {
+            setting.value = Container.p[setting.key]
+        }   
     }
 
+    //generate the setting elements
     for (let i = 0; i < Object.keys(settings.s).length; i++) {
         const key = Object.keys(settings.s)[i];
         const val = settings.s[key]
